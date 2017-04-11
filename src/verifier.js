1ecf46d002333b570439153d3876087630e89f08
Honglai <Honglai.Zhan@Vodafone.com.au>
2017-04-11 12:21:46 +0800

    [honglai] revert to utf8

--- src/verifier.js
+++ src/verifier.js
@@ -68,17 +68,13 @@ Verifier.prototype.verify = function () {
 
 	this._instance = cp.spawn(file, args, opts);
 
-	this._instance.stdout.setEncoding('UTF-8');
-	console.log("------------");
+	this._instance.stdout.setEncoding('utf8');
 	this._instance.stdout.on('data', outputHandler);
-	this._instance.stderr.setEncoding('UTF-8');
+	this._instance.stderr.setEncoding('utf8');
 	this._instance.stderr.on('data', outputHandler);
 	this._instance.on('error', logger.error.bind(logger));
 
 	this._instance.once('close', function (code) {
-		console.log('close method');
-		console.log(code);
-		console.log(output);
 		code == 0 ? deferred.resolve(output) : deferred.reject(new Error(output));
 	});
 
